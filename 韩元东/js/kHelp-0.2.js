9ae85c90694fd974b91ab609a3be614770b717a2
kavinhan <xiaoyu11.03@gmail.com>
2013-03-19 11:18:50 +0800

    Upload 03-19 morning homework
    upload fixAd.html
    add To kHelp-0.2.js
    $getScroll()

--- 韩元东/js/kHelp-0.2.js
+++ 韩元东/js/kHelp-0.2.js
@@ -1,44 +1,97 @@
-/*
- * Use ID name get node
+/**
  * Author: Kavin
  * Time: 2013年3月14日13:08:55
- * Use: $id('IDName'); // return node IDName
+ */
+/**
+ * doc -> document
+ */
+var doc = document;
+/**
+ * return id node
+ * @param  {string} id -> id name
+ * @return {node}   id node
  */
 function $id( id ) {
-	return document.getElementById(id);
+	return doc.getElementById(id);
 }
-/*
- * Use Tag name get nodelist
- * Author: Kavin
- * Time: 2013年3月14日13:08:55
- * Use1: $tag('TagName'); // return nodelist TagName;
- * Use2: $tag('TagName',ID); // return ID node children Tags of TagName nodelist 
+/**
+ * return tag nodelist
+ * @param  {string} tag  -> tag name
+ * @param  {node} parent -> parent node name
+ * @return {nodelist}    tags nodelist
  */
 function $tag( tag, parent ) {
 	if ( parent ) {
 		return parent.getElementsByTagName(tag);
 	} else {
-		return document.getElementsByTagName(tag);
+		return doc.getElementsByTagName(tag);
 	}
 }
-/*
- * Author: Kavin
+/**
+ * receive obj and return the split this obj.className to an array
+ * @param  {node} obj -> node
+ * @return {array}    -> className array
+ */
+function $classToArray( obj ) {
+	// object class name
+	var ocn = obj.className,
+		arr = ocn.split(' ');
+	return arr;
+}
+/**
+ * delete all obj className
+ * @param  {node} obj -> node
  */
 function $emptyClass( obj ) {
 	obj.className = '';
 }
+/**
+ * add one and more classname to obj
+ * @param {node} obj  -> node
+ * @param {string} classname -> "classnameA + classnameB + ..."
+ */
 function $addClass( obj, classname ) {
-	var ocn = obj.className;
-	if ( ocn ) {
-		obj.className = ocn + ' ' + classname;
-	} else {
-		obj.className = classname;
+	var arr  = $classToArray( obj ),  //array obj.className
+		temp = true,
+		str  = '',
+		arrc = classname.split(' '),  // classname array
+		arrcl = $size(arrc);  //classname array length
+		// console.log("arrc->"+arrc);
+		// console.log("arrcl->"+arrcl);
+	if ( arrcl === 1 ) {
+		for ( var i = 0; i < $size(arr); i++ ) {
+			if ( arr[i] === classname ) {
+				temp = false;
+			}
+		}
+		if ( temp ) {
+			obj.className = $trim(obj.className + ' ' + classname);
+		}
+	} 
+	else if ( arrcl > 1 ) {
+		for ( var i = 0; i < arrcl; i++ ) {
+			for ( var j = 0; j < $size(arr); j++ ) {
+				if ( arrc[i] === arr[j] ) {
+					console.log("false")
+					temp = false;
+				}
+			}
+			if ( temp ) {
+				str += arrc[i] + ' ';
+			}
+			temp = true;
+		}
+		obj.className = $trim(obj.className + ' ' + str);
 	}
 }
+/**
+ * remove obj class classname
+ * @param  {node} obj   -> node
+ * @param  {string} classname -> classname
+ */
 function $removeClass( obj, classname ) {
-	var ocn = obj.className;
-	var arr = ocn.split(' ');
-	var str = '';
+	var arr = $classToArray( obj ),
+		str = '';
 	for ( var i = 0; i < $size(arr); i++ ) {
 		if ( arr[i] !== classname ) {
 			str += arr[i] + ' ';
@@ -46,10 +99,79 @@ function $removeClass( obj, classname ) {
 	}
 	obj.className = $trim(str);
 }
+/**
+ * change obj old class name to new class name
+ * @param  {node} obj   -> node
+ * @param  {string} oldClass -> old class name
+ * @param  {string} newClass -> new class name
+ */
+function $changeClass( obj, oldClass, newClass ) {
+	var arr = $classToArray( obj ),
+		str = '';
+	for ( var i = 0; i < $size(arr); i++ ) {
+		if ( arr[i] === oldClass ) {
+			arr[i] = newClass;
+			str += arr[i] + ' ';
+		} else {
+			str += arr[i] + ' ';
+		}
+	}
+	obj.className = $trim(str);
+}
+/**
+ * delect obj first and last space
+ * @param  {node} obj -> node
+ * @return {obj}  return new obj
+ */
 function $trim( obj ) {
 	return obj.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
 }
-/*
+/**
+ * Author: Kavin
+ * [$setStyle description]
+ * @param {[type]} obj  [description]
+ * @param {[type]} sty  [description]
+ * @param {[type]} num  [description]
+ * @param {[type]} unit [description]
+ */
+function $setStyle( obj, sty, num, unit ) {
+	if ( unit ) {
+		obj.style[sty] = num + unit;
+	} else {
+		obj.style[sty] = num;
+	}
+}
+/**
+ * get Scroll Top and Left length
+ * if no obj return body scroll
+ * if has obj return obj scrll
+ * @param  {string} direction -> only 'Top' and 'Left'
+ * @param  {node} obj   -> node
+ * @return {number}     -> scroll lenth number
+ */
+function $getScroll ( direction, obj ) {
+	if ( obj ) {
+		switch ( direction ) {
+			case 'Top':
+				var ret = obj.scrollTop;
+				break;
+			case 'Left':
+				var ret = obj.scrollLeft;
+				break;
+		}
+	} else {
+		switch ( direction ) {
+			case 'Top':
+				var ret = doc.documentElement.scrollTop || doc.body.scrollTop
+				break;
+			case 'Left':
+				var ret = doc.documentElement.scrollLeft || doc.body.scrollLeft
+				break;
+		}
+	}
+	return ret;
+}
+/**
  Author: Mr.Chen
  */
 function $tweenEffect( obj, sty, end, options ) {
@@ -59,22 +181,20 @@ function $tweenEffect( obj, sty, end, options ) {
 	    } else {
 	        var d = options.d;
 	    }
-
 	    if ( !options.twFn ) {
 	        var twFn = "Quad";
 	    } else {
 	        var twFn = options.twFn;
 	    }
-
 	    if ( !options.easeFn ) {
 	        var easeFn = "easeOut";
 	    } else {
 	        var easeFn = options.easeFn;
 	    }   
 	} else {
-	    var d = 31;
-	    var twFn = 'Quad';
-	    var easeFn = 'easeOut';
+	    var d = 31,
+	    	twFn = 'Quad',
+	    	easeFn = 'easeOut';
 	}
 	var b = $getStyle(obj, sty);
 	//字符串=>数字
@@ -83,8 +203,8 @@ function $tweenEffect( obj, sty, end, options ) {
 	if( isNaN(b) ){
 	    b = 0;
 	}
-	var c = end - b;
-	var t = 0;
+	var c = end - b,
+		t = 0;
 	if ( !obj.t ) {
 	    obj.t = {};
 	}
@@ -107,7 +227,7 @@ function $tweenEffect( obj, sty, end, options ) {
 	    }
 	},17);
 }
-/*
+/**
  * Author: Blue;
  */
 function $getStyle( obj, style ) {
@@ -118,16 +238,6 @@ function $getStyle( obj, style ) {
 	}
 }
 
-function $setStyle( obj, sty, num, unit ) {
-	// console.log("obj->" + obj + '\n' + "sty->" + sty + '\n' + "num->" + num + '\n' + "unit->" + unit)
-	if ( unit ) {
-		// console.log("true")
-		obj.style[sty] = num + unit;
-	} else {
-		// console.log("false")
-		obj.style[sty] = num;
-	}
-}
 
 
 
@@ -147,7 +257,7 @@ function $setStyle( obj, sty, num, unit ) {
 
 
 
-/*
+/**
  * Author: Jquery
  */
 function $size( obj ) {
